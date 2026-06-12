ff4329596a27ba62ee6a9233fcc14f8ac089ee26
pad <git@depad.fr>
2026-06-12 16:47:22 +0200

    fix: update h5ai configuration to enforce root user, improve cache directory creation, and silence shell command stderr

--- slash/usr/local/bin/init_perms.sh
+++ slash/usr/local/bin/init_perms.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo "Setting permissions for h5ai cache directories..."
+mkdir -p /usr/share/h5ai/_h5ai/public/cache /usr/share/h5ai/_h5ai/private/cache
 chown -R nginx:www-data /usr/share/h5ai/_h5ai/public/cache /usr/share/h5ai/_h5ai/private/cache
 chmod -R 755 /usr/share/h5ai/_h5ai/public/cache /usr/share/h5ai/_h5ai/private/cache
